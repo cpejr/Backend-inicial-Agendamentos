bbc46350774bb9b0afe6499774ffdaeb20d6a13b
Rafael <rafaelcampellosoares@gmail.com>
2024-11-14 15:39:02 -0300

    Descrição clara do que foi alterado

--- src/Loaders/mongodb.js
+++ src/Loaders/mongodb.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 async function startDB(){
-await mongoose.connect('mongodb+srv://rcampellosoares243:sLKdVuISHIcWEGad@clusternode.ziuld.mongodb.net/')
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Banco de dados inicializado");
 }
 
 module.exports = startDB;
