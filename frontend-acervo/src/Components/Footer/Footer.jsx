71dd5a5edb05ea0e01fb0eb25ed44cfadb0f6d8b
Rafael <rafaelcampellosoares@gmail.com>
2025-02-04 11:19:48 -0300

    feat: inicials configurations :rocket:

--- frontend-acervo/src/Components/Footer/Footer.jsx
+++
@@ -1,3 +0,0 @@
-export default function Footer() {
-    return <h3>Footer</h3>;
-}
--- frontend-acervo/src/layouts/AppLayout/Styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-    min-height: 100vh;
-    min-height: 100dvh;
-    
-    display: grid;
-    grid-template-rows: auto 1fr;
-`;
